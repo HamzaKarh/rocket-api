9e05a40004d20e50287bc81b4ed6b31769e4a7a5
Hamza <karhat.hamza1@gmail.com>
2022-07-12 17:47:58 -0400

    Transferring to new setup

--- src/entities/files/file.rs
+++ src/entities/files/file.rs
@@ -7,7 +7,11 @@ use db_connect::DbConn;
 use diesel::query_dsl::methods::OrderDsl;
 use diesel::{ExpressionMethods, RunQueryDsl};
 use rocket::{self, delete, get, post};
+use rocket::http::hyper::header::ContentType;
+use rocket::http::uri::Path;
+use rocket::response::content::Html;
 use rocket_contrib::json::Json;
+use rocket_upload::MultipartDatas;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Queryable, Serialize)]
@@ -25,9 +29,10 @@ pub struct NewFile {
 }
 
 #[post("/create", data = "<new_file>")]
-pub fn create_file(conn: DbConn, new_file: Json<NewFile>) -> Json<File> {
+pub fn create_file(conn: DbConn, new_file: MultipartDatas) -> Json<File> {
+    let file_fields = new_file.texts;
     let result = diesel::insert_into(files::table)
-        .values(&new_file.0)
+        .values(&file_fields)
         .get_result(&*conn)
         .unwrap();
     Json(result)
@@ -41,4 +46,23 @@ pub fn get_all_files(conn: DbConn) -> Json<Vec<File>> {
         .unwrap();
 
     Json(result)
+}
+
+#[post("/upload", data = "<data>")]
+fn upload(content_type: &ContentType, data: MultipartDatas) -> Html<String> {
+    // to get a param from client
+    // let mut result=format!("UserID:{}<br>",userid);
+    // content_type not used here, just for more informations
+    let mut result = format!("{:?}<br>",content_type);
+    // aquire all Form field data
+    for t in data.texts {
+    result = format!("{}FieldName:{} --- FieldValue:{}<br>",result,t.key,t.value);
+    }
+    // aquire all files upload
+    for f in data.files {
+    result = format!("{}FieldName:{} --- FileName:{} --- StoragePath:{}<br>",
+      result,f.name,f.filename,f.path);
+    f.persist(Path::new("upload"));
+    }
+    Html(format!("<html><head></head><body>upload coming...<br>{}</body></html>",result))
 }
